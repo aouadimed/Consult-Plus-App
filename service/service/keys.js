10b84415f827ac5bdcdf006e6511cedf40f73e08
aouadimed <mohamed.aouadi@esprit.tn>
2022-11-12 19:27:32 +0100

    service

---
+++ service/service/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://amine005:xPLoUMVL5W7kXCNB@cluster0.yq7o4us.mongodb.net/ConsultDatabase?retryWrites=true&w=majority",
+  JWT_SECRET: "xPLoUMVL5W7kXCNB",
+};
